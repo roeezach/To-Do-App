e692c9bd0106b00536f1371afd387eab480eaefb
roeez <Roeezach15@gmail.com>
2023-01-14 22:50:01 +0200

    fix CR Comments

--- to-do-app/src/components/Input.js
+++ to-do-app/src/components/Input.js
@@ -2,53 +2,54 @@ import React, { useReducer } from "react";
 import { useState } from "react";
 
 const Input = (props) => {
-    const [content, setContent] = useState('')
+  const [content, setContent] = useState("");
 
-    const handleChange = (event) => {
-        setContent(event.target.value)
+  const handleChange = (event) => {
+    setContent(event.target.value);
+  };
+
+  const validateText = (text) => {
+    if (!text) return false;
+    if (text.length < 3) return false;
+
+    return true;
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      const isTextValid = validateText(event.target.value);
+      if (isTextValid) {
+        onAddTodo();
+        setContent("");
+        console.log(content);
+      } else {
+        alert("your text can't be empty");
+      }
     }
+  };
 
-    const validateText = (text) => {
-        let isTextValid = false
-        if (text !== '') {
-            isTextValid = true
-        }
-        else {
-            isTextValid = false;
-        }
+  const onAddTodo = () => {
+    const newToDo = {
+      content: content,
+      isCompleted: false,
+      id: Math.random(),
+    };
+    props.setTodos([...props.todos, newToDo]);
+  };
 
-        return isTextValid
-    }
+  return (
+    <div>
+      <input
+        placeholder="Please type your todo here"
+        type="text"
+        id="textInput"
+        className="textInput"
+        onKeyDown={handleKeyDown}
+        onChange={handleChange}
+        value={content}
+      ></input>
+    </div>
+  );
+};
 
-    const handleKeyDown = (event) => {
-        if (event.key === 'Enter') {
-            let isTextValid = validateText(event.target.value)
-            if (isTextValid) {
-                onAddTodo()
-                setContent('')
-                console.log(content);
-            }
-            else {
-                alert("your text can't be empty")
-            }
-        }
-    }
-
-    const onAddTodo = () => {
-        const newToDo = {
-            content: content, isCompleted: false, id: Math.random()
-        }
-        props.setTodos([...props.todos, newToDo])
-    }
-
-
-
-
-    return (
-        <div>
-            <input placeholder="Please type your todo here" type="text" id="textInput" className="textInput" onKeyDown={handleKeyDown} onChange={handleChange} value={content} ></input>
-        </div>
-    )
-}
-
-export default Input
+export default Input;
--- to-do-app/src/components/TodoItem.js
+++ to-do-app/src/components/TodoItem.js
@@ -1,25 +1,23 @@
 import React from "react";
-import IconCross from "../images/icon-cross.svg"
-import IconCheck from "../images/icon-check.svg"
+import IconCross from "../images/icon-cross.svg";
+import IconCheck from "../images/icon-check.svg";
 
-const TodoItem = (props) => {
-
-    return (
-        <div className="todo-Item-wrapper" ismarked={`${props.isCompleted}`}>
-            <div className="todo-Item">
-                <button className="check-btn" type="submit" onClick={props.onToggle}>
-                    <img src={IconCheck} alt="logo" />
-                </button>
-                <div className="todo-item-content">
-                    {props.content}
-                </div>
-            </div>
-            <div className="remove-btn">
-                <button onClick={props.onRemove}>
-                    <img src={IconCross} alt="logo" />
-                </button>
-            </div>
-        </div>)
-}
+const TodoItem = ({ content, isCompleted, onToggle, onRemove }) => {
+  return (
+    <div className="todo-Item-wrapper" ismarked={`${isCompleted}`}>
+      <div className="todo-Item">
+        <button className="check-btn" type="submit" onClick={onToggle}>
+          <img src={IconCheck} alt="logo" />
+        </button>
+        <div className="todo-item-content">{content}</div>
+      </div>
+      <div className="remove-btn">
+        <button onClick={onRemove}>
+          <img src={IconCross} alt="logo" />
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default TodoItem;
